4a57cba90b81ceb5d119ecea2ac7f8ca73b5dfe6
NobleAscent <25895859+NobleAscent@users.noreply.github.com>
2023-02-25 19:26:20 +0100

    BASE: Update far righ symbols

---
+++ crkbd3/keymap/keycodes.h
@@ -0,0 +1,15 @@
+#pragma once
+
+enum layers {
+    L_BASE,
+    L_SYMBOLS,
+    L_SHORTCUTS,
+    L_RGB,
+};
+
+#define SHORT_SPC LT(2, KC_SPC)
+
+#define TMUX LGUI(KC_1)
+#define BROWSER LGUI(KC_1)
+#define INTELLIJ LGUI(KC_1)
+#define VSCODE LGUI(KC_1)
